eecd3f5fb37e523a425f328edc971a57d26bfadc
JiaShuo Zou <3490581068@qq.com>
2025-10-18 23:17:48 +0800

    小更改

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -8,6 +8,12 @@ export default {
         date: "2025-10-18 17:41",
         tags: ["日常"],
         content :"今天开始搭建个人博客网站，记录一些生活和学习的点滴，希望能坚持下去！"
+      },
+      {
+        date: "2025-10-18 22:50",
+        tags: ["更新"],
+        content : '增添了评论功能，大家可以在文章下方留言交流了！<p class="vh-img-flex"><img src="https://zycs-img-2lg.pages.dev/v2/rhddNSq.png"></p>'
+
       }
   ]
 }
