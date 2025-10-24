0a7f90c91e7495693d3eefe9015745bc636b67a0
JiaShuo Zou <3490581068@qq.com>
2025-10-24 18:46:19 +0800

    更改说说顺序

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -5,19 +5,19 @@ export default {
   // 注意：图片请用 vh-img-flex 类包裹
   data: [
       {
-        date: "2025-10-18 17:41",
+        date: "2025-10-24 18:26",
         tags: ["日常"],
-        content :"今天开始搭建个人博客网站，记录一些生活和学习的点滴，希望能坚持下去！"
+        content :'今天是10.24，祝所有程序员节日快乐！<p>从清晨的 Alert，到深夜的 Commit;</p><p>从天花乱坠的需求文档，到严丝合缝的运行上线。</p><p>世界有裂缝，TA 们用代码缝。</p><p>可是，修完了今天的 bug,记得也要补一补自己。</p><p> 🎉 1024，节日快乐！</p><p>💙 愿所有重启都能顺利启动</p><p>💜 愿代码也能回报你的坚持</p><p>🩷 愿温柔与你同频运行</p><p>💛 愿世界也能温柔回应你</p><p>💚 愿你也给自己打个“更新补丁”</p><p class="vh-img-flex"><img src="https://zycs-img-2lg.pages.dev/v2/TkSIVzx.jpeg"></p>'
       },
       {
         date: "2025-10-18 22:50",
         tags: ["更新"],
         content : '增添了评论功能，大家可以在文章下方留言交流了！<p class="vh-img-flex"><img src="https://zycs-img-2lg.pages.dev/v2/rhddNSq.png"></p>'
       },
-      {
-        date: "2025-10-24 18:26",
+       {
+        date: "2025-10-18 17:41",
         tags: ["日常"],
-        content :'今天是10.24，祝所有程序员节日快乐！<p>从清晨的 Alert，到深夜的 Commit;</p><p>从天花乱坠的需求文档，到严丝合缝的运行上线。</p><p>世界有裂缝，TA 们用代码缝。</p><p>可是，修完了今天的 bug,记得也要补一补自己。</p><p> 🎉 1024，节日快乐！</p><p>💙 愿所有重启都能顺利启动</p><p>💜 愿代码也能回报你的坚持</p><p>🩷 愿温柔与你同频运行</p><p>💛 愿世界也能温柔回应你</p><p>💚 愿你也给自己打个“更新补丁”</p><p class="vh-img-flex"><img src="https://zycs-img-2lg.pages.dev/v2/TkSIVzx.jpeg"></p>'
+        content :"今天开始搭建个人博客网站，记录一些生活和学习的点滴，希望能坚持下去！"
       }
   ]
 }
